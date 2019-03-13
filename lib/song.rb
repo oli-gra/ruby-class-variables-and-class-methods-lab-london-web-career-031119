536ac2ab94ea11c62a93edb00a7a7ca3066f7c42
Oliver <oliver.grant@gmail.com>
2019-03-13 17:45:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song 
+
+attr_accessor :name,:artist,:genre
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize (name,artist,genre)
+  @name = name
+  @@count += 1
+  @artist = artist
+  @@artists << artist
+  @genre = genre
+  @@genres << genre
+end
+
+def self.count
+  @@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.artist_count
+  hash_count = Hash.new(0)
+  @@artists.each do |artist|
+    hash_count[artist] += 1
+  end
+  hash_count
+end
+
+def self.genre_count
+  hash_count = Hash.new(0)
+  @@genres.each do |genre|
+    hash_count[genre] += 1
+  end
+  hash_count
+end
+
+end
